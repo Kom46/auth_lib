61bb7024d3ecf2d9855dda51f84883a5efca614e
kom46 <kommunizm46@gmail.com>
2022-03-23 18:32:11 +0300

    added w25qxx save/load data functions

--- src/platform_depended.c
+++ src/platform_depended.c
@@ -17,3 +17,41 @@ __attribute__ ((weak)) int load_buff_from_volatile_mem(void* ptr, size_t size)
   return (int)0;
 }
 
+#if defined(W25QXX)
+#include "w25qxx.h"
+
+// flash start address for storing user information
+#define FLASH_START_ADDR 0x000000
+// flash end address for storing user information
+#define FLASH_END_ADDR 0x000F00
+
+int save_buff_to_volatile_mem(void* ptr, size_t size)
+{
+  int result = 0;
+  if ((FLASH_START_ADDR + size) <= FLASH_END_ADDR)
+  {
+    W25qxx_EraseSector(FLASH_START_ADDR);
+    W25qxx_WriteSector(ptr, FLASH_START_ADDR, 0, (uint32_t)size);
+  }
+  else
+  {
+    result = -1;
+  }
+  return result;
+}
+
+int load_buff_from_volatile_mem(void* ptr, size_t size)
+{
+  int result = 0;
+  if ((FLASH_START_ADDR + size) <= FLASH_END_ADDR)
+  {
+    W25qxx_ReadSector(ptr, FLASH_START_ADDR, 0, (uint32_t)size);
+  }
+  else
+  {
+    result = -1;
+  }
+  return result;
+}
+
+#endif
